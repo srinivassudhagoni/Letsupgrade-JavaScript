136ed7a30fb4b6de35c317ecdf72166beb80538c
srinivassudhagoni <48527966+srinivassudhagoni@users.noreply.github.com>
2020-09-03 19:18:00 +0530

    day2 assignment

---
+++ day2.js
@@ -0,0 +1,35 @@
+// 1.Program to search for a particular character in a string
+
+  // let string = 'helicopter';
+  // let search = string.indexOf("o");
+  // console.log("position of character is"+ " " +  search);
+
+// 2.Program to convert minutes to seconds
+
+  // let a = 5;
+  // let b;
+  // if ( b= a*60 ){
+  //     console.log(b +"seconds");
+  // }
+
+// 3.Program to search for a element in a array of strings
+
+  // let vechile = ['car','bus','train','ship','cycle','bike'];
+  // let search = vechile.includes('train');
+  // console.log(search);
+
+// 4.program to display only elements containing 'a'  in them from a array
+
+  // let apps = ['Instagram','Facebook','Google','LinkedIn','Gmail','Amazon']
+  // for(let i=0 ; i<= apps.length ; i++){
+  //   if(apps[i].includes('a')){
+  //     console.log(apps[i]);
+  //   }
+  // };
+
+// 5.print array in reverse order
+
+  // let fruits = ['apple','strawberry','banana','mango','kiwi','pineapple','papaya'];
+  //  for( let i=0; i<fruits.length; i++){
+  //     console.log( fruits.reverse()[i]);
+  //  } 
